3d45e99dba178a2ab19661ec73ac2f7d5ce63464
algernon <aeneas@aloku.net>
2024-03-21 17:23:11 +1100

    Add project files.

---
+++ Code/Mod.cs
@@ -0,0 +1,64 @@
+﻿// <copyright file="Mod.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// Licensed under the Apache Licence, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace PlopTheGrowables
+{
+    using System.Reflection;
+    using Colossal.Logging;
+    using Game;
+    using Game.Buildings;
+    using Game.Modding;
+
+    /// <summary>
+    /// The base mod class for instantiation by the game.
+    /// </summary>
+    public sealed class Mod : IMod
+    {
+        /// <summary>
+        /// The mod's default name.
+        /// </summary>
+        public const string ModName = "Plop the Growables";
+
+        /// <summary>
+        /// Gets the active instance reference.
+        /// </summary>
+        public static Mod Instance { get; private set; }
+
+        /// <summary>
+        /// Gets the mod's active log.
+        /// </summary>
+        internal ILog Log { get; private set; }
+
+        /// <summary>
+        /// Called by the game when the mod is loaded.
+        /// </summary>
+        /// <param name="updateSystem">Game update system.</param>
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            // Set instance reference.
+            Instance = this;
+
+            // Initialize logger.
+            Log = LogManager.GetLogger(ModName);
+#if DEBUG
+            Log.Info("setting logging level to Debug");
+            Log.effectivenessLevel = Level.Debug;
+#endif
+            Log.Info($"loading {ModName} version {Assembly.GetExecutingAssembly().GetName().Version}");
+
+            updateSystem.World.GetOrCreateSystemManaged<ZoneCheckSystem>().Enabled = false;
+        }
+
+        /// <summary>
+        /// Called by the game when the mod is disposed of.
+        /// </summary>
+        public void OnDispose()
+        {
+            Log.Info("disposing");
+            Instance = null;
+        }
+    }
+}
